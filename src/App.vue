9884deb4c5e885964fafdfb40158137edfa7b246
Hyemi-Lee <hello7hm@gmail.com>
2021-08-21 12:26:04 +0900

    Create project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>Hello Vue!</h1>
+</template>
+
+<script>
+export default {
+    date() {
+        return {
+            msg: 'Vue!'
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+h1 {
+    color: red;
+}
+</style>
